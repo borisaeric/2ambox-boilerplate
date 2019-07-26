3b3b701eb400b83b93983c5f067ba11af22491c3
borisaeric <borisa123@hotmail.com>
2019-07-26 21:31:36 +0200

    remove dummy databases

--- 2ambox/provision.sh
+++ 2ambox/provision.sh
@@ -1,14 +1,18 @@
 echo 'START ----> Running provision script <----'
 
-mysql -u root -e "CREATE DATABASE 2amigos CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-echo '---> Created 2amigos database'
+mysql -u root -e "CREATE DATABASE php50 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+echo '---> Created php50 database'
 
-mysql -u root -e "CREATE DATABASE 2amigos_track CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
-echo '---> Created 2amigos_track database'
+mysql -u root -e "CREATE DATABASE php50 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+echo '---> Created php70 database'
 
-mysql -u root -e "CREATE USER 'healthtechhq'@'%' IDENTIFIED BY '';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON 2amigos_track.* TO 'healthtechhq'@'%' IDENTIFIED BY '';"
-mysql -u root -e "FLUSH PRIVILEGES;"
-echo '---> Created user for healthtechhq project which uses this db'
+mysql -u root -e "CREATE DATABASE php50 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+echo '---> Created php71 database'
+
+mysql -u root -e "CREATE DATABASE php50 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+echo '---> Created php72 database'
+
+mysql -u root -e "CREATE DATABASE php50 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+echo '---> Created php73 database'
 
 echo 'END ----> Running provision script <----'
